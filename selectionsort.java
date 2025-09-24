82500d8dd2660f4d626e1144c326719185923575
Devansh Verma <vermadevansh0001@gmail.com>
2025-09-25 02:22:02 +0530

    selectionsort

--- selectionsort.java
+++ selectionsort.java
@@ -1,43 +1,37 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class selectionsort{
 
-    public static void sort(int n, int[] arr){
-        for(int i = 0;i<n-1;i++){
+    public static void sort(int n,int arr[]) {
+        for(int i = 0;i<n-2;i++){
             int min = i;
-            for(int j = i+1;j<n;j++){
+            for(int j = i;j<n-1;j++){
                 if(arr[j]<arr[min]){
                     min = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
+        
     }
-
-    public static void main(String[] args){
-         Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
         int n = sc.nextInt();
 
-        int[] arr = new int[n];
-        
-        System.out.println("Enter "+ n + " elements: ");
-        for(int i = 0; i < n; i++){
+        int arr[] = new int[n];
+
+        for(int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
-
         sort(n, arr);
+        System.out.println("Sorted Array: ");
 
-        System.out.println("The elements are : ");
-        for(int i = 0; i < n; i++){
+        for(int i = 0;i<n;i++){
             System.out.println(arr[i]);
         }
+        sc.close();
     }
-
-
-
-    
 }
